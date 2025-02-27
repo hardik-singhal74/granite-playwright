cf05747aa9287aefcf0f40c1ebbca3ceea30be5e
Hardik Singhal <hardik.singhal@bigbinary.com>
2025-02-27 18:33:57 +0530

    Refactored code using hooks and added code for cleaning up test artifacts

--- playwright-tests/e2e/poms/tasks.ts
+++ playwright-tests/e2e/poms/tasks.ts
@@ -1,20 +1,46 @@
 import {Page, expect} from '@playwright/test';
 
+interface TaskName {
+  taskName: string;
+}
+interface CreateNewTaskProps extends TaskName {
+  userName ?: string;
+}
+
 export class TaskPage {
   page: Page;
   constructor(page:Page){
     this.page = page;
   }
-  createTaskAndVerify = async ({taskName}:{taskName: string}) => {
+  createTaskAndVerify = async ({taskName, userName = "Oliver Smith"}:CreateNewTaskProps) => {
     await this.page.getByTestId('navbar-add-todo-link').click();
     await this.page.getByTestId('form-title-field').fill(taskName);
     await this.page.locator('.css-2b097c-container').click();
-    await this.page.locator(".css-26l3qy-menu").getByText("Oliver Smith").click();
+    await this.page.locator(".css-26l3qy-menu").getByText(userName).click();
     await this.page.getByTestId('form-submit-button').click();
     const taskInDashboard = this.page.getByTestId('tasks-pending-table').getByRole("row",{
       name: new RegExp(taskName,"i"),
     });
     await taskInDashboard.scrollIntoViewIfNeeded();
     await expect(taskInDashboard).toBeVisible();
+  };
+  markTaskAsCompletedAndVerify = async ({taskName}:{taskName: string}) => {
+    await expect(this.page.getByRole("heading",{name : "Loading..."})).toBeHidden();
+
+    const completedTaskInDashboard = this.page.getByTestId("tasks-completed-table").getByRole("row" , {name: new RegExp(taskName,"i")});
+    const isTaskCompleted = await completedTaskInDashboard.count();
+
+    if(isTaskCompleted) return;
+
+    await this.page.getByTestId('tasks-pending-table').getByRole("row" , {name: taskName}).getByRole("checkbox").click();
+    await completedTaskInDashboard.scrollIntoViewIfNeeded();
+    await expect(completedTaskInDashboard).toBeVisible();
+  };
+  starTaskAndVerify = async ({taskName}: {taskName: string}) => {
+    const starIcon = this.page.getByTestId('tasks-pending-table').getByRole('row', { name: new RegExp(taskName,"i")}).getByTestId('pending-task-star-or-unstar-link');
+    await starIcon.click();
+    await expect(starIcon).toHaveClass(/ri-star-fill/i);
+    const taskRow = this.page.getByTestId("tasks-pending-table").getByRole("row").nth(1);
+    await expect(taskRow).toContainText(new RegExp(taskName,"i"));
   };
 }
--- playwright-tests/e2e/tests/tasks.spec.ts
+++ playwright-tests/e2e/tests/tasks.spec.ts
@@ -1,21 +1,80 @@
-import { test } from "../fixtures"
+import { test } from "../fixtures";
 import { faker } from "@faker-js/faker";
 import { expect } from "@playwright/test";
+import { TaskPage } from "../poms/tasks";
+import LoginPage from "../poms/login";
 
-test.describe("Tasks Page", ()=>{
-  let taskName : string;
-  test.beforeEach(async ({page})=>{
-    taskName = faker.word.words({count: 5});
+test.describe("Tasks Page", () => {
+
+  let taskName: string;
+
+  test.beforeEach(async ({ page, taskPage }, testInfo) => {
+    taskName = faker.word.words({ count: 5 });
+
+    if (testInfo.title.includes("[SKIP_SETUP]")) return;
+
     await page.goto("/");
-  });
-  test("should create a new task with creator as the assignee", async ({page, taskPage}) =>{
     await taskPage.createTaskAndVerify({taskName});
   });
-  test("should be able to mark a test as completed", async ({page, loginPage, taskPage}) => {
-    await taskPage.createTaskAndVerify({taskName});
-    await page.getByTestId('tasks-pending-table').getByRole("row",{name: taskName}).getByRole("checkbox").click();
-    const completedTaskInDashboard = page.getByTestId('tasks-completed-table').getByRole("row",{name: taskName});
-    await completedTaskInDashboard.scrollIntoViewIfNeeded();
-    await expect(completedTaskInDashboard).toBeVisible();
+
+  test.afterEach(async ({ page , taskPage }) => {
+    await page.goto("/");
+    await taskPage.markTaskAsCompletedAndVerify({taskName});
+    const completedTaskInDashboard = await page
+      .getByTestId("tasks-completed-table")
+      .getByRole("row", { name: new RegExp(taskName, "i") });
+    await completedTaskInDashboard
+      .getByTestId("completed-task-delete-link")
+      .click();
+    await expect(completedTaskInDashboard).toBeHidden();
+    await expect(
+      page
+        .getByTestId("tasks-pending-table")
+        .getByRole("row", { name: new RegExp(taskName, "i") })
+    ).toBeHidden();
+  });
+
+  test("should be able to mark a task as completed", async ({
+    page,
+    taskPage,
+  }) => {
+    await taskPage.markTaskAsCompletedAndVerify({ taskName });
+  });
+
+  test.describe("Starring task feature", () => {
+    test.describe.configure({ mode: "serial" });
+    test("should be able to star a task", async ({ page, taskPage }) => {
+      await taskPage.starTaskAndVerify({ taskName });
+    });
+    test("should be able to un-star a task", async ({ page, taskPage }) => {
+      await taskPage.starTaskAndVerify({ taskName });
+      const starIcon = page
+        .getByTestId("tasks-pending-table")
+        .getByRole("row", { name: new RegExp(taskName, "i") })
+        .getByTestId("pending-task-star-or-unstar-link");
+      await starIcon.click();
+      await expect(starIcon).toHaveClass(/ri-star-fill/);
+    });
+  });
+  test("should add a new task with a different user as the assignee [SKIP_SETUP]", async ({page, browser, taskPage})=>{
+    await page.goto("/");
+    await taskPage.createTaskAndVerify({taskName, userName: "Sam Smith"});
+
+    const newUserContext = await browser.newContext({
+      storageState: {cookies:[],origin:[]},
+    });
+    const newUserPage = await newUserContext.newPage();
+
+    const loginPage = new LoginPage(newUserPage);
+
+    await newUserPage.goto("/");
+    await loginPage.loginAndVerifyUser({
+      email: "sam@example.com",
+      password: "welcome",
+      username: "Sam Smith",
+    });
+    await expect( newUserPage.getByTestId("tasks-pending-table").getByRole("row",{name: taskName})).toBeVisible();
+    await newUserPage.close();
+    await newUserContext.close();
   });
 });
