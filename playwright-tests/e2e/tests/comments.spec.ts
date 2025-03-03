b3e3c1efad3ac0822805176e0d25c4f0d1946b11
Hardik Singhal <hardik.singhal@bigbinary.com>
2025-03-03 17:04:03 +0530

    Simplified login test step by removing unnecessary async/await

--- playwright-tests/e2e/tests/comments.spec.ts
+++ playwright-tests/e2e/tests/comments.spec.ts
@@ -28,17 +28,17 @@ test.describe("Adding comments to a task", () => {
   };
 
   test.beforeEach(async ({ page, loginPage, taskPage }) => {
-    await test.step("Step 1: Going to the home page", async () => await page.goto("/"));
-    await test.step("Step 2: Generating task and comment", async () =>{
+    await test.step("Step 1: Going to the home page", () => page.goto("/"));
+    test.step("Step 2: Generating task and comment", () =>{
       taskName = faker.word.words({ count: 5 });
       comment = faker.word.words({ count: 5 });
     });
-    await test.step("Step 3: Logging in as creator", async ()=> await loginPage.loginAndVerifyUser({
+    await test.step("Step 3: Logging in as creator", ()=> loginPage.loginAndVerifyUser({
       email: creator.email,
       password: creator.password,
       username: creator.name,
     }));
-    await test.step("Step 4: Creating a task", async ()=> await taskPage.createTaskAndVerify({ taskName, userName: assignee.name }));
+    await test.step("Step 4: Creating a task", ()=> taskPage.createTaskAndVerify({ taskName, userName: assignee.name }));
     await test.step("Step 5: Scrolling to the task", async ()=> {
       const taskInDashboard = await page.getByTestId(TASKS_TABLE_SELECTORS.pendingTasksTable).getByText(taskName);
       await taskInDashboard.scrollIntoViewIfNeeded();
@@ -46,10 +46,8 @@ test.describe("Adding comments to a task", () => {
   });
 
   test.afterEach(async ({ page, loginPage, taskPage }) => {
-    await test.step("Step 1: Logining in as the creator", async ()=> await loginPage.logoutAndLoginAsDifferentUser({ email: creator.email, password: creator.password, username: creator.name }));
-    await test.step("Step 2: Marking the task as completed", async ()=>
-      await taskPage.markTaskAsCompletedAndVerify({ taskName })
-    );
+    await test.step("Step 1: Logining in as the creator", ()=> loginPage.logoutAndLoginAsDifferentUser({ email: creator.email, password: creator.password, username: creator.name }));
+    await test.step("Step 2: Marking the task as completed", ()=> taskPage.markTaskAsCompletedAndVerify({ taskName }));
     await test.step("Step 3: Deleting the task", async ()=>{
       const completedTaskInDashboard = await page.getByTestId(TASKS_TABLE_SELECTORS.completedTasksTable).getByRole("row", { name: new RegExp(taskName, "i") });
       await completedTaskInDashboard.getByTestId(TASKS_TABLE_SELECTORS.deleteTaskButton).click();
@@ -59,7 +57,7 @@ test.describe("Adding comments to a task", () => {
   });
 
   test("should check if it persists", async ({ page, commentPage }) => {
-    await test.step("Step 1: Adding a comment to the task", async ()=> await commentPage.addCommentToTaskAndVerify({ taskName, comment }));
+    await test.step("Step 1: Adding a comment to the task", ()=> commentPage.addCommentToTaskAndVerify({ taskName, comment }));
     await test.step("Step 2: Checking if the comment persists", async ()=> {
       const commentInTask = await page.getByTestId(CREATE_COMMENT_SELECTORS.commentField).getByText(comment);
       await expect(commentInTask).toBeVisible();
@@ -67,9 +65,7 @@ test.describe("Adding comments to a task", () => {
   });
 
   test("should check if comment appears to the assignee", async ({ page, loginPage, taskPage, commentPage }) => {
-    await test.step("Step 1: Logging in as the assignee and adding a comment to the task", async ()=> {
-      await commentPage.addCommentThenLogoutAndLoginAsDifferentUser({ taskName, comment, email: assignee.email, password: assignee.password, username: assignee.name })
-    });
+    await test.step("Step 1: Logging in as the assignee and adding a comment to the task", ()=> commentPage.addCommentThenLogoutAndLoginAsDifferentUser({ taskName, comment, email: assignee.email, password: assignee.password, username: assignee.name }));
     await test.step("Step 2: Checking if the comment appears to the assignee", async ()=> {
       await page.getByTestId(TASKS_TABLE_SELECTORS.pendingTasksTable).getByText(taskName).click();
       const commentInTask = await page.getByTestId(CREATE_COMMENT_SELECTORS.commentField).getByText(comment);
@@ -78,7 +74,7 @@ test.describe("Adding comments to a task", () => {
   });
 
   test("should persist when added by the assignee", async ({ page, loginPage, taskPage, commentPage }) => {
-    await test.step("Step 1: Logging in as the assignee", async () => await loginPage.logoutAndLoginAsDifferentUser({ email: assignee.email, password: assignee.password, username: assignee.name }));
+    await test.step("Step 1: Logging in as the assignee", () => loginPage.logoutAndLoginAsDifferentUser({ email: assignee.email, password: assignee.password, username: assignee.name }));
     await test.step("Step 2: Adding a comment to the task and verify", async () => {
       await commentPage.addCommentToTaskAndVerify({ taskName, comment });
       const commentInTask = await page.getByTestId(CREATE_COMMENT_SELECTORS.commentField).getByText(comment);
@@ -87,7 +83,7 @@ test.describe("Adding comments to a task", () => {
   });
 
   test("should be visible to the creator when added by the assignee", async ({page, loginPage, taskPage, commentPage})=>{
-    await test.step("Step 1: Logging in as the assignee", async () => await loginPage.logoutAndLoginAsDifferentUser({ email: assignee.email, password: assignee.password, username: assignee.name }));
+    await test.step("Step 1: Logging in as the assignee", () => loginPage.logoutAndLoginAsDifferentUser({ email: assignee.email, password: assignee.password, username: assignee.name }));
     await test.step("Step 2: Adding a comment to the task then login as creator and verify", async () => {
       await commentPage.addCommentThenLogoutAndLoginAsDifferentUser({ taskName, comment, email: creator.email, password: creator.password, username: creator.name});
       await page.getByTestId(TASKS_TABLE_SELECTORS.pendingTasksTable).getByText(taskName).click();
@@ -96,69 +92,41 @@ test.describe("Adding comments to a task", () => {
     });
   });
 
-  test("should check if comment count increases", async ({ page, taskPage, commentPage}) => {
+  test.describe("should check if comment count increases", () => {
     let commentCount: Promise<string>, commentCountAfterAddingTask: Promise<string>;
-    await test.step("Step 1: Counting the number of Comments", async () => {
-      commentCount = await page.getByRole('row', { name: new RegExp(taskName,"i")}).getByRole('cell').nth(3).innerText();
+    test.beforeEach( async ({page, taskPage, commentPage}) => {
+      await test.step("Step 1: Counting the number of Comments", async () => {
+        commentCount = await page.getByRole('row', { name: new RegExp(taskName,"i")}).getByRole('cell').nth(3).innerText();
+      });
     });
-    await test.step("Step 2: Adding new comments", async () => await commentPage.addCommentToTaskAndVerify({ taskName, comment }));
-    await test.step("Step 3: Counting the number of comments", async ()=> {
-      await page.getByTestId(NAVBAR_SELECTORS.todoPageButton).click();
-      commentCountAfterAddingTask = await page.getByRole('row', { name: new RegExp(taskName,"i")}).getByRole('cell').nth(3).innerText();
+    test.afterEach(async ({ page }) => {
+      await test.step("Step 1: Checking if the comment count increased", async ()=>{
+        commentCountAfterAddingTask = await page.getByRole('row', { name: new RegExp(taskName,"i")}).getByRole('cell').nth(3);
+        await expect(commentCountAfterAddingTask).toHaveText( String( parseInt(commentCount) + 1) );
+      });
     });
-    await test.step("Step 4: Checking if the comment count increased", async ()=>{
-      const beforeCount = await parseInt(commentCount);
-      const afterCount = await parseInt(commentCountAfterAddingTask);
-      await expect(afterCount).toBe(beforeCount + 1);
+    test("for the user", async ({ page, taskPage, commentPage}) => {
+      await test.step("Step 1: Adding new comments", () => commentPage.addCommentToTaskAndVerify({ taskName, comment }));
+      await test.step("Step 2: Counting the number of comments", ()=> page.getByTestId(NAVBAR_SELECTORS.todoPageButton).click());
     });
-  });
 
-  test("should check if comment count increases for the assignee", async ({ page, loginPage, taskPage, commentPage }) => {
-    let commentCount: Promise<string>, commentCountAfterAddingTask: Promise<string>;
-    await test.step("Step 1: Counting the number of Comments", async ()=>{
-      commentCount = await page.getByRole('row', { name: new RegExp(taskName,"i")}).getByRole('cell').nth(3).innerText();
+    test("for the assignee", async ({ page, loginPage, taskPage, commentPage }) => {
+      await test.step("Step 1: Adding comment and then verifying comment count as assignee", () => commentPage.addCommentThenLogoutAndLoginAsDifferentUser({ taskName, comment, email: assignee.email, password: assignee.password, username: assignee.name }));
     });
-    await test.step("Step 2: Adding comment and then verifying comment count as assignee", async () => {
-      await commentPage.addCommentThenLogoutAndLoginAsDifferentUser({ taskName, comment, email: assignee.email, password: assignee.password, username: assignee.name });
-      commentCountAfterAddingTask = await page.getByRole('row', { name: new RegExp(taskName,"i")}).getByRole('cell').nth(3).innerText();
-      const beforeCount = await parseInt(commentCount);
-      const afterCount = await parseInt(commentCountAfterAddingTask);
-      await expect(afterCount).toBe(beforeCount + 1);
-    });
-  });
 
-  test("should increase comment count when added by the assignee", async ({ page, loginPage, taskPage, commentPage }) => {
-    let commentCount: Promise<string>, commentCountAfterAddingTask: Promise<string>;
-    await test.step("Step 1: Counting the number of comments", async () => {
-      commentCount = await page.getByRole('row', { name: new RegExp(taskName,"i")}).getByRole('cell').nth(3).innerText();
+    test("when added by the assignee", async ({ page, loginPage, taskPage, commentPage }) => {
+      await test.step("Step 1: Adding comments as an assignee", async () => {
+        await loginPage.logoutAndLoginAsDifferentUser({ email: assignee.email, password: assignee.password, username: assignee.name });
+        await commentPage.addCommentToTaskAndVerify({ taskName, comment });
+      });
+      await test.step("Step 2: Checking if the comment count increases", ()=> page.getByTestId(NAVBAR_SELECTORS.todoPageButton).click());
     });
-    await test.step("Step 2: Adding comments as an assignee", async () => {
-      await loginPage.logoutAndLoginAsDifferentUser({ email: assignee.email, password: assignee.password, username: assignee.name });
-      await commentPage.addCommentToTaskAndVerify({ taskName, comment });
-    });
-    await test.step("Step 3: Checking if the comment count increases", async ()=> {
-      await page.getByTestId(NAVBAR_SELECTORS.todoPageButton).click();
-      commentCountAfterAddingTask = await page.getByRole('row', { name: new RegExp(taskName,"i")}).getByRole('cell').nth(3).innerText();
-      const beforeCount = await parseInt(commentCount);
-      const afterCount = await parseInt(commentCountAfterAddingTask);
-      await expect(afterCount).toBe(beforeCount + 1);
-    });
-  });
 
-  test("should increase comment count for the creator when added by the assignee", async ({page, loginPage, taskPage, commentPage})=>{
-    let commentCount: Promise<string>, commentCountAfterAddingTask: Promise<string>;
-    await test.step("Step 1: Counting the number of comments", async () => {
-      commentCount = await page.getByRole('row', { name: new RegExp(taskName,"i")}).getByRole('cell').nth(3).innerText();
-    });
-    await test.step("Step 2: Adding comment as an assignee and logging in as a creator", async () => {
-      await loginPage.logoutAndLoginAsDifferentUser({ email: assignee.email, password: assignee.password, username: assignee.name });
-      await commentPage.addCommentThenLogoutAndLoginAsDifferentUser({ taskName, comment, email: creator.email, password: creator.password, username: creator.name});
-    });
-    await test.step("Step 3: Checking if comment count increases for the creator", async () => {
-      commentCountAfterAddingTask = await page.getByRole('row', { name: new RegExp(taskName,"i")}).getByRole('cell').nth(3).innerText();
-      const beforeCount = await parseInt(commentCount);
-      const afterCount = await parseInt(commentCountAfterAddingTask);
-      await expect(afterCount).toBe(beforeCount + 1);
+    test("for the creator when added by the assignee", async ({page, loginPage, taskPage, commentPage})=>{
+      await test.step("Step 1: Adding comment as an assignee and logging in as a creator", async () => {
+        await loginPage.logoutAndLoginAsDifferentUser({ email: assignee.email, password: assignee.password, username: assignee.name });
+        await commentPage.addCommentThenLogoutAndLoginAsDifferentUser({ taskName, comment, email: creator.email, password: creator.password, username: creator.name});
+      });
     });
   });
 });
--- playwright-tests/e2e/tests/login.setup.ts
+++ playwright-tests/e2e/tests/login.setup.ts
@@ -6,9 +6,7 @@ import { COMMON_TEXTS } from "../constants/texts";
 test.describe("Login Page",()=>{
   test("should login with correct credentials", async ({page, loginPage})=>{
     await test.step("Step 1: Visit login page", () => page.goto("/"));
-    await test.step("Step 2: Login and verify admin user", async () => {
-      await loginPage.loginAndVerifyUser({email: process.env.ADMIN_EMAIL!, password: process.env.ADMIN_PASSWORD!, username: COMMON_TEXTS.defaultUserName, });
-    });
+    await test.step("Step 2: Login and verify admin user", () => loginPage.loginAndVerifyUser({email: process.env.ADMIN_EMAIL!, password: process.env.ADMIN_PASSWORD!, username: COMMON_TEXTS.defaultUserName}));
     await page.context().storageState({ path: STORAGE_STATE });
   });
 });
